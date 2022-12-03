dad255734e3719b11a04aff4e40c982711da5371
Douglas <cbdouglasvirissimo@hotmail.com>
2022-12-03 14:59:12 -0300

    6º Requisito

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const memeText = document.getElementById('meme-text');
 const textInput = document.getElementById('text-input');
 const memeImage = document.getElementById('meme-image');
 const memeInsert = document.getElementById('meme-insert');
+const memeImageContainer = document.getElementById('meme-image-container');
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
 
 const insertTextPrev = () => {
   textInput.addEventListener('input', (e) => {
@@ -12,9 +16,18 @@ const insertTextPrev = () => {
 const insertImagePrev = () => {
   memeInsert.addEventListener('change', (e) => {
     memeImage.src = URL.createObjectURL(e.target.files[0]);
-    console.log(memeImage.src);
-  });
-};
+  })
+}
+
+const changeBorder = (e) => {
+  memeImageContainer.className = e.target.className;
+}
+
+fire.addEventListener('click', changeBorder);
+water.addEventListener('click', changeBorder);
+earth.addEventListener('click', changeBorder);
+
+
 
 insertImagePrev();
 insertTextPrev();
