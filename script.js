00bb9bb4d14fbba110ef484eeed1675da9ab90ea
Douglas <cbdouglasvirissimo@hotmail.com>
2022-12-02 23:47:24 -0300

    2º Requisito

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const memeText = document.getElementById('meme-text');
-const inputText = document.getElementById('text-input');
+const textInput = document.getElementById('text-input');
+const memeImage = document.getElementById('meme-image');
+const memeInsert = document.getElementById('meme-insert');
 
-const insertPrev = () => {
-  inputText.addEventListener('input', (e) => {
+const insertTextPrev = () => {
+  textInput.addEventListener('input', (e) => {
     memeText.innerHTML = e.target.value;
   });
 };
 
-insertPrev();
+const insertImagePrev = () => {
+  memeInsert.addEventListener('change', (e) => {
+    memeImage.src = URL.createObjectURL(e.target.files[0]);
+    console.log(memeImage.src);
+  });
+};
+
+insertImagePrev();
+insertTextPrev();
